87e02fe80864e3f768ee29009d5e22feb8532267
kenneth-fernandes <kenneth.peter.fernandes@gmail.com>
2020-10-29 14:46:07 -0400

    Kenneth: Added JavaServer and FileSTorehandler code.

---
+++ java/src/FileStoreHandler.java
@@ -0,0 +1,34 @@
+
+import org.apache.thrift.TException;
+
+import chord.*;
+
+public class FileStoreHandler implements Filestore.Iface {
+    private RFile rFile;
+    private NodeID nodeId = new NodeID();
+
+    public void writeFile(RFile rFile) {
+        this.rFile = rFile;
+    }
+
+    public RFile readFile(java.lang.String filename) {
+        return rFile;
+    }
+
+    public void setFingertable(java.util.List<NodeID> node_list) {
+        System.out.println("setFingertable()");
+    }
+
+    public NodeID findSucc(java.lang.String key) {
+        return nodeId;
+    }
+
+    public NodeID findPred(java.lang.String key) {
+        return nodeId;
+    }
+
+    public NodeID getNodeSucc() {
+        return nodeId;
+    }
+    
+}
--- java/src/JavaServer.java
+++ java/src/JavaServer.java
@@ -1,3 +1,59 @@
+
+import org.apache.thrift.server.TServer;
+import org.apache.thrift.server.TServer.Args;
+import org.apache.thrift.server.TSimpleServer;
+import org.apache.thrift.server.TThreadPoolServer;
+import org.apache.thrift.transport.TSSLTransportFactory;
+import org.apache.thrift.transport.TServerSocket;
+import org.apache.thrift.transport.TServerTransport;
+import org.apache.thrift.transport.TSSLTransportFactory.TSSLTransportParameters;
+
+import chord.*;
+
+
 public class JavaServer {
     
+    public static FileStoreHandler handler;
+    public static FileStoreHandler.Processor processor;
+    public static int port;
+
+  public static void main(String [] args) {
+    try {
+      handler = new FileStoreHandler();
+      processor = new FileStoreHandler.Processor(handler);
+      port= Integer.valueOf(args[0]);
+      Runnable simple = new Runnable() {
+        public void run() {
+          simple(processor);
+        }
+      };      
+      /*
+      Runnable secure = new Runnable() {
+        public void run() {
+          secure(processor);
+        }
+      };
+      */
+
+      new Thread(simple).start();
+      // new Thread(secure).start();
+    } catch (Exception x) {
+      x.printStackTrace();
+    }
+  }
+
+  public static void simple(FileStoreHandler.Processor processor) {
+    try {
+      TServerTransport serverTransport = new TServerSocket(9090);
+      TServer server = new TSimpleServer(new Args(serverTransport).processor(processor));
+
+      // Use this for a multithreaded server
+      // TServer server = new TThreadPoolServer(new TThreadPoolServer.Args(serverTransport).processor(processor));
+
+      System.out.println("Starting the simple server...");
+      server.serve();
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+  }
 }
